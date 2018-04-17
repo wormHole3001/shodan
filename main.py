85518fbe849488dc74bbcfaaabf31c667340dbf8
Fernando Gonzalez <28124633+wormHole3001@users.noreply.github.com>
2018-04-16 20:21:15 -0500

    Create main.py
    - created main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from help_functions import *
+from termcolor import colored,cprint
+
+print("Shodan:")
+cprint("The world's first search engine for Internet-connected devices",'cyan')
+cprint("Created by: Fernando Gonzalez",'magenta',attrs=['blink'])
+print("[+] You'll need your API key from shodan.io")
+print("--------------------------------------------------------------")
+main_menu()
